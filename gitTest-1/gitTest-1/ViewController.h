bc6b899459e48f0d58635b658fa42c9affbc0916
youyoumacmini3 <youyoumacmini3@youyoumacmini3demac-mini.local>
2016-06-13 10:44:03 +0800

    gitTest

---
+++ gitTest-1/gitTest-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest-1
+//
+//  Created by youyoumacmini3 on 16/6/13.
+//  Copyright © 2016年 youyoumacmini3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest-1/gitTest-1/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  gitTest-1
+//
+//  Created by youyoumacmini3 on 16/6/13.
+//  Copyright © 2016年 youyoumacmini3. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+}
+-(void)test{
+    NSLog(@"hehe ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
